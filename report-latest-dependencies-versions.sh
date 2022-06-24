1a8d2cf3f35022699a5d4408d54e4bdf10ea1e2d
Simon Levesque <simon@foilen.com>
2022-06-24 10:07:34 -0400

    Update libraries

--- report-latest-dependencies-versions.sh
+++ report-latest-dependencies-versions.sh
@@ -5,5 +5,5 @@ set -e
 RUN_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $RUN_PATH
 
-echo ----[ Compile ]----
+echo ----[ Report ]----
 ./gradlew dependencyUpdates --refresh-dependencies | tee _report-latest.txt
