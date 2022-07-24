84899fffa65bc6dfb40932739bd416e5b7bd78ff
Simon Levesque <simon@foilen.com>
2022-07-24 12:16:55 +0000

    Add missing copyrights

--- src/main/java/com/foilen/clouds/manager/services/model/DnsConfig.java
+++ src/main/java/com/foilen/clouds/manager/services/model/DnsConfig.java
@@ -1,3 +1,12 @@
+/*
+    Foilen Clouds Manager
+    https://github.com/foilen/foilen-clouds-manager
+    Copyright (c) 2021-2022 Foilen (https://foilen.com)
+
+    The MIT License
+    http://opensource.org/licenses/MIT
+
+ */
 package com.foilen.clouds.manager.services.model;
 
 import com.foilen.smalltools.tools.AbstractBasics;
