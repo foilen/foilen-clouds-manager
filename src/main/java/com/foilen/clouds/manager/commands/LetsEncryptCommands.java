0a5865a3f32c4d9fde312493ade0d89f2e7d13d6
Simon Levesque <simon@foilen.com>
2023-02-08 00:07:03 +0000

    Update copyright

--- src/main/java/com/foilen/clouds/manager/commands/LetsEncryptCommands.java
+++ src/main/java/com/foilen/clouds/manager/commands/LetsEncryptCommands.java
@@ -1,7 +1,7 @@
 /*
     Foilen Clouds Manager
     https://github.com/foilen/foilen-clouds-manager
-    Copyright (c) 2021-2022 Foilen (https://foilen.com)
+    Copyright (c) 2021-2023 Foilen (https://foilen.com)
 
     The MIT License
     http://opensource.org/licenses/MIT
