688b3099af93f617b26ad6139e433ea9ce64234f
Simon Levesque <simon@foilen.com>
2025-03-11 17:25:33 -0400

    Copyright

--- src/main/java/com/foilen/clouds/manager/commands/DnsCommands.java
+++ src/main/java/com/foilen/clouds/manager/commands/DnsCommands.java
@@ -1,7 +1,7 @@
 /*
     Foilen Clouds Manager
     https://github.com/foilen/foilen-clouds-manager
-    Copyright (c) 2021-2023 Foilen (https://foilen.com)
+    Copyright (c) 2021-2025 Foilen (https://foilen.com)
 
     The MIT License
     http://opensource.org/licenses/MIT
